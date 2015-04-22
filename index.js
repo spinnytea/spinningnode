bb80b95628f508339b05de1dac9c63df99ff465a
Garrett Tjarks <spinnytea@gmail.com>
2015-04-21 22:14:12 -0400

    stubbing out where the maze stuff will go

--- index.js
+++ index.js
@@ -1,15 +1,20 @@
 'use strict';
 require('angular').module('spinningnode', [
+  require('./lib/maze/mazeModule').name,
   'ngRoute'
 ])
-  .config(['$routeProvider', function($routeProvider) {
-    $routeProvider.otherwise({
-      templateUrl: 'template/menu.html'
-    });
-  }])
-  .factory('$exceptionHandler', function() {
-    return function(exception, cause) {
-      if(cause) { exception.message += ' (caused by "' + cause + '")'; }
-      throw exception;
-    };
+.config(['$routeProvider', function($routeProvider) {
+  $routeProvider.when(
+    '/mazes', {
+      templateUrl: 'template/maze/app.html'
+    }
+  ).otherwise({
+    templateUrl: 'template/menu.html'
   });
+}])
+.factory('$exceptionHandler', function() {
+  return function(exception, cause) {
+    if(cause) { exception.message += ' (caused by "' + cause + '")'; }
+    throw exception;
+  };
+});
