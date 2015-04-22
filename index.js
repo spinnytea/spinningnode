816c389f6ad959a5267feeb35ff99a1609c1b70a
Garrett Tjarks <spinnytea@gmail.com>
2015-04-21 22:03:28 -0400

    git a basic route/view setup
    at least it works

--- index.js
+++ index.js
@@ -1,9 +1,15 @@
 'use strict';
 require('angular').module('spinningnode', [
+  'ngRoute'
 ])
-.factory('$exceptionHandler', function() {
-  return function(exception, cause) {
-    if(cause) { exception.message += ' (caused by "' + cause + '")'; }
-    throw exception;
-  };
-});
+  .config(['$routeProvider', function($routeProvider) {
+    $routeProvider.otherwise({
+      templateUrl: 'template/menu.html'
+    });
+  }])
+  .factory('$exceptionHandler', function() {
+    return function(exception, cause) {
+      if(cause) { exception.message += ' (caused by "' + cause + '")'; }
+      throw exception;
+    };
+  });
