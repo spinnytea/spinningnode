b1be44bc5d24ead06cc9098fdd8fdf416fa45176
Garrett Henderson-Tjarks <spinnytea@gmail.com>
2016-03-09 21:22:19 -0500

    init battle sudoku

--- index.js
+++ index.js
@@ -2,20 +2,20 @@
 angular.module('spinningnode', [
   require('./lib/maze/mazeModule').name,
   require('./lib/utils/datadiffModule').name,
+  require('./lib/battlesudoku/index').name,
   'ngRoute'
 ])
 .config(['$routeProvider', function($routeProvider) {
-  $routeProvider.when(
-    '/mazes', {
-      controller: 'spinningnode.mazes.appController',
-      templateUrl: 'template/maze/app.html'
-    }
-  ).when(
-    '/datadiff', {
-      controller: 'spinningnode.utils.diff.controller',
-      templateUrl: 'template/utils/datadiff.html'
-    }
-  ).otherwise({
+  $routeProvider.when('/mazes', {
+    controller: 'spinningnode.mazes.appController',
+    templateUrl: 'template/maze/app.html'
+  }).when('/datadiff', {
+    controller: 'spinningnode.utils.diff.controller',
+    templateUrl: 'template/utils/datadiff.html'
+  }).when('/bs', {
+      controller: 'spinningnode.battlesudoku.app.controller',
+      templateUrl: 'template/battlesudoku/app.html'
+  }).otherwise({
     templateUrl: 'template/menu.html'
   });
 }])
