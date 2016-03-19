c7e1854edbb08cd18c3a2ba91a210043fd59c3d9
Garrett Henderson-Tjarks <spinnytea@gmail.com>
2016-03-19 15:28:58 -0400

    update auto to account for squares marked 'empty'

--- test/battlesudoku/auto.js
+++ test/battlesudoku/auto.js
@@ -58,27 +58,27 @@ describe('auto', function() {
 
     it('availableBoard', function() {
       var a = auto.units.availableBoard(1, 1);
-      expect(a).to.deep.equal([[true]]);
+      expect(a).to.deep.equal([['none']]);
 
       a = auto.units.availableBoard(2, 3);
-      expect(a).to.deep.equal([[true, true, true],[true, true, true]]);
+      expect(a).to.deep.equal([['none', 'none', 'none'],['none', 'none', 'none']]);
 
 
       var b = board.initBoard([1, 2, 1], [1, 0, 3], [3, 1]);
       a = auto.units.availableBoard(b);
       expect(a).to.deep.equal([
-        [true, true, true],
-        [true, true, true],
-        [true, true, true],
+        ['none', 'none', 'none'],
+        ['none', 'none', 'none'],
+        ['none', 'none', 'none'],
       ]);
 
       b[0][2].state = 'fill';
       b[1][2].state = 'fill';
       a = auto.units.availableBoard(b);
       expect(a).to.deep.equal([
-        [true, true, false],
-        [true, true, false],
-        [true, true, true],
+        ['none', 'none', 'fill'],
+        ['none', 'none', 'fill'],
+        ['none', 'none', 'none'],
       ]);
     });
 
@@ -89,9 +89,9 @@ describe('auto', function() {
       expect(c.row).to.deep.equal([0, 0]);
       expect(c.col).to.deep.equal([0, 0, 0]);
 
-      a[0][0] = false;
-      a[0][1] = false;
-      a[1][1] = false;
+      a[0][0] = 'fill';
+      a[0][1] = 'fill';
+      a[1][1] = 'fill';
       c =  auto.units.getCounts(a);
 
       expect(c.row).to.deep.equal([2, 1]);
@@ -111,7 +111,7 @@ describe('auto', function() {
       expect(auto.units.canPlace(a, 1, 1, 5, true)).to.equal(false);
       expect(auto.units.canPlace(a, 1, 1, 5, false)).to.equal(false);
 
-      a[3][3] = false;
+      a[3][3] = 'fill';
 
       expect(auto.units.canPlace(a, 0, 0, 4, true)).to.equal(true);
       expect(auto.units.canPlace(a, 1, 0, 4, true)).to.equal(true);
@@ -127,11 +127,20 @@ describe('auto', function() {
       expect(auto.units.canPlace(a, 0, 2, 2, false)).to.equal(true);
       expect(auto.units.canPlace(a, 0, 3, 2, false)).to.equal(true);
 
+      a[1][1] = 'empty';
+
+      expect(auto.units.canPlace(a, 0, 0, 4, true)).to.equal(true);
+      expect(auto.units.canPlace(a, 1, 0, 4, true)).to.equal(false);
+      expect(auto.units.canPlace(a, 2, 0, 2, true)).to.equal(true);
+      expect(auto.units.canPlace(a, 0, 0, 3, false)).to.equal(true);
+      expect(auto.units.canPlace(a, 0, 1, 3, false)).to.equal(false);
+      expect(auto.units.canPlace(a, 0, 2, 2, false)).to.equal(true);
+
       expect(a).to.deep.equal([
-        [true, true, true, true],
-        [true, true, true, true],
-        [true, true, true, true],
-        [true, true, true, false]
+        ['none', 'none', 'none', 'none'],
+        ['none', 'empty', 'none', 'none'],
+        ['none', 'none', 'none', 'none'],
+        ['none', 'none', 'none', 'fill']
       ]);
     });
 
@@ -140,26 +149,26 @@ describe('auto', function() {
 
       auto.units.doPlace(a, 0, 0, 4, true);
       expect(a).to.deep.equal([
-        [false, false, false, false],
-        [true, true, true, true],
-        [true, true, true, true],
-        [true, true, true, true]
+        ['fill', 'fill', 'fill', 'fill'],
+        ['none', 'none', 'none', 'none'],
+        ['none', 'none', 'none', 'none'],
+        ['none', 'none', 'none', 'none']
       ]);
 
       auto.units.doPlace(a, 2, 0, 2, false);
       expect(a).to.deep.equal([
-        [false, false, false, false],
-        [true, true, true, true],
-        [false, true, true, true],
-        [false, true, true, true]
+        ['fill', 'fill', 'fill', 'fill'],
+        ['none', 'none', 'none', 'none'],
+        ['fill', 'none', 'none', 'none'],
+        ['fill', 'none', 'none', 'none']
       ]);
 
       auto.units.doPlace(a, 2, 2, 1, false);
       expect(a).to.deep.equal([
-        [false, false, false, false],
-        [true, true, true, true],
-        [false, true, false, true],
-        [false, true, true, true]
+        ['fill', 'fill', 'fill', 'fill'],
+        ['none', 'none', 'none', 'none'],
+        ['fill', 'none', 'fill', 'none'],
+        ['fill', 'none', 'none', 'none']
       ]);
     });
 
@@ -168,7 +177,7 @@ describe('auto', function() {
 
     it('findRowLengths', function() {
       var a = auto.units.availableBoard(4, 3);
-      a[3][2] = false;
+      a[3][2] = 'fill';
 
       expect(auto.units.findRowLengths(a, 1)).to.deep.equal([{r:1,c:0,l:3,d:true}]);
       expect(auto.units.findRowLengths(a, 2)).to.deep.equal([{r:2,c:0,l:1,d:true}]);
@@ -181,7 +190,7 @@ describe('auto', function() {
 
     it('findColLengths', function() {
       var a = auto.units.availableBoard(4, 3);
-      a[3][2] = false;
+      a[3][2] = 'fill';
 
       expect(auto.units.findColLengths(a, 0)).to.deep.equal([{r:0,c:0,l:4,d:false}]);
       expect(auto.units.findColLengths(a, 2)).to.deep.equal([{r:0,c:2,l:2,d:false}]);
@@ -194,7 +203,7 @@ describe('auto', function() {
 
     it('findAllLengths', function() {
       var a = auto.units.availableBoard(4, 3);
-      a[3][2] = false;
+      a[3][2] = 'fill';
       expect(auto.units.findAllLengths(a)).to.deep.equal([{r:0,c:0,l:4,d:false}]);
       expect(auto.units.findAllLengths(a, 3)).to.deep.equal([
         {r:0,c:0,l:3,d:true}, {r:1,c:0,l:3,d:true},
@@ -210,9 +219,9 @@ describe('auto', function() {
     });
 
     it('recursiveSolve', function() {
-      expect(auto.units.recursiveSolve([[false]], [], {row:[0],col:[0]})).to.deep.equal([[false]]);
-      expect(auto.units.recursiveSolve([[true]], [1], {row:[1],col:[1]})).to.deep.equal([[false]]);
-      expect(auto.units.recursiveSolve([[true, true]], [2], {row:[2],col:[1, 1]})).to.deep.equal([[false, false]]);
+      expect(auto.units.recursiveSolve([['fill']], [], {row:[0],col:[0]})).to.deep.equal([['fill']]);
+      expect(auto.units.recursiveSolve([['none']], [1], {row:[1],col:[1]})).to.deep.equal([['fill']]);
+      expect(auto.units.recursiveSolve([['none', 'none']], [2], {row:[2],col:[1, 1]})).to.deep.equal([['fill', 'fill']]);
     });
 
     it('updateRecursiveCounts', function() {
@@ -224,7 +233,7 @@ describe('auto', function() {
 
     it('updateBoard', function() {
       var b = board.initBoard([1, 0], [1, 0], [1]);
-      auto.units.updateBoard([[false, true], [true, true]], b);
+      auto.units.updateBoard([['fill', 'none'], ['none', 'none']], b);
       expect(b.map(function(r) { return _.map(r, 'state'); })).to.deep.equal([
         ['fill', 'empty'],
         ['empty', 'empty'],
