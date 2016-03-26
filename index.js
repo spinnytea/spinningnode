6483f7277aa9d0bd3803393ae788507c3414103e
Garrett Henderson-Tjarks <spinnytea@gmail.com>
2016-03-26 11:52:42 -0400

    settings page

--- index.js
+++ index.js
@@ -14,8 +14,11 @@ angular.module('spinningnode', [
     controller: 'spinningnode.utils.diff.controller',
     templateUrl: 'utils/datadiff.html'
   }).when('/bs', {
-      controller: 'spinningnode.battlesudoku.app.controller',
-      templateUrl: 'battlesudoku/app.html'
+    controller: 'spinningnode.battlesudoku.app.controller',
+    templateUrl: 'battlesudoku/app.html'
+  }).when('/bs/settings', {
+    controller: 'spinningnode.battlesudoku.settings.controller',
+    templateUrl: 'battlesudoku/settings.html'
   }).otherwise({
     templateUrl: 'menu.html'
   });
