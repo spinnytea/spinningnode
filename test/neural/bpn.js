248c30c2b72d650356523352838112008bcab584
Garrett Henderson-Tjarks <spinnytea@gmail.com>
2016-11-18 18:21:59 -0500

    feed forward

--- test/neural/bpn.js
+++ test/neural/bpn.js
@@ -3,6 +3,33 @@ const expect = require('chai').expect;
 const NeuralNetwork = require('../../lib/neural/bpn');
 
 describe.only('bpn', function() {
+  describe('NeuralNetwork', function() {
+    it('constructor', function() {
+      const nn = new NeuralNetwork(3, 2, 1);
+      expect(nn.input).to.deep.equal([0,0,0]);
+      expect(nn.hidden).to.deep.equal([0,0]);
+      expect(nn.output).to.deep.equal([0]);
+    });
+
+    it('feed', function() {
+      const nn = new NeuralNetwork(2, 1, 2);
+      nn.ih[0][0] = 2;
+      nn.ih[1][0] = 3;
+      nn.ho[0][0] = 4;
+      nn.ho[0][1] = 5;
+
+      nn.feed([1, 2]);
+
+      // these values work without normalization
+      // XXX I'm not sure what to test with normalization on
+      // expect(nn.input).to.deep.equal([1, 2]);
+      // expect(nn.ih).to.deep.equal([[2], [3]]);
+      // expect(nn.hidden).to.deep.equal([8]);
+      // expect(nn.ho).to.deep.equal([[4, 5]]);
+      // expect(nn.output).to.deep.equal([32, 40]);
+    });
+  }); // end NeuralNetwork
+
   describe('units', function() {
     it('matrix', function() {
       expect(NeuralNetwork.units.matrix(2, 3)).to.deep.equal([[0,0,0],[0,0,0]]);
