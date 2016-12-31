8556b74777c01c15382c0dbd08840ed95d52b599
Garrett Henderson-Tjarks <spinnytea@gmail.com>
2016-12-30 19:08:48 -0500

    regroup to basics

---
+++ test/saomvp/skills.js
@@ -0,0 +1,54 @@
+'use strict';
+const _ = require('lodash');
+const expect = require('chai').expect;
+const Skill = require('../../lib/saomvp/skills');
+
+describe('saomvp', function() {
+  describe('skills', function() {
+    function curr(s) { return _.pick(s, ['curr', 'level']); }
+
+    it('add 2', function() {
+      let s = new Skill('test 2', 2);
+      expect(curr(s)).to.deep.equal({ curr: 0, level: 1 });
+      s.add();
+      expect(curr(s)).to.deep.equal({ curr: 1, level: 1 });
+      s.add();
+      expect(curr(s)).to.deep.equal({ curr: 0, level: 2 });
+      s.add(3);
+      expect(curr(s)).to.deep.equal({ curr: 3, level: 2 });
+      s.add();
+      expect(curr(s)).to.deep.equal({ curr: 0, level: 3});
+    });
+
+    it('add 100', function() {
+      let s = new Skill('test 100', 100);
+      expect(curr(s)).to.deep.equal({ curr: 0, level: 1 });
+      s.add();
+      expect(curr(s)).to.deep.equal({ curr: 1, level: 1 });
+      s.add(98);
+      expect(curr(s)).to.deep.equal({ curr: 99, level: 1 });
+      s.add();
+      expect(curr(s)).to.deep.equal({ curr: 0, level: 2 });
+      s.add(199);
+      expect(curr(s)).to.deep.equal({ curr: 199, level: 2 });
+      s.add();
+      expect(curr(s)).to.deep.equal({ curr: 0, level: 3 });
+    });
+
+    it('add previous', function() {
+      let s = new Skill('test 2', 2);
+      s.previous_level = 3;
+      expect(curr(s)).to.deep.equal({ curr: 0, level: 1 });
+      s.add();
+      expect(curr(s)).to.deep.equal({ curr: 0, level: 2 });
+      s.add();
+      expect(curr(s)).to.deep.equal({ curr: 2, level: 2 });
+      s.add();
+      expect(curr(s)).to.deep.equal({ curr: 0, level: 3 });
+      s.add();
+      expect(curr(s)).to.deep.equal({ curr: 1, level: 3 });
+    });
+
+    it.skip('add large w/ previous');
+  }); // end skills
+}); // end saomvp
