7f14bea0e1716797b4fc81d92b368eae6fea4b6f
Garrett Henderson-Tjarks <spinnytea@gmail.com>
2016-12-31 13:59:47 -0500

    skills, equip slots

--- test/saomvp/skills.js
+++ test/saomvp/skills.js
@@ -9,7 +9,7 @@ describe('saomvp', function() {
       function curr(s) { return _.pick(s, ['curr', 'level']); }
 
       it('2', function() {
-        let s = new Skill('test 2', 2);
+        let s = new Skill('test 2', 'desc 2', 2, 10);
         expect(curr(s)).to.deep.equal({ curr: 0, level: 1 });
         s.add();
         expect(curr(s)).to.deep.equal({ curr: 1, level: 1 });
@@ -22,7 +22,7 @@ describe('saomvp', function() {
       });
 
       it('100', function() {
-        let s = new Skill('test 100', 100);
+        let s = new Skill('test 100', 'desc 100', 100, 10);
         expect(curr(s)).to.deep.equal({ curr: 0, level: 1 });
         s.add();
         expect(curr(s)).to.deep.equal({ curr: 1, level: 1 });
@@ -37,7 +37,7 @@ describe('saomvp', function() {
       });
 
       it('previous', function() {
-        let s = new Skill('test previous', 2);
+        let s = new Skill('test previous', 'desc previous', 2, 10);
         s.previous_level = 3;
         expect(curr(s)).to.deep.equal({ curr: 0, level: 1 });
         s.add();
@@ -50,8 +50,19 @@ describe('saomvp', function() {
         expect(curr(s)).to.deep.equal({ curr: 1, level: 3 });
       });
 
+      it('max', function() {
+        let s = new Skill('test 2', 'desc 2', 2, 10);
+        expect(curr(s)).to.deep.equal({ curr: 0, level: 1 });
+        s.add(89);
+        expect(curr(s)).to.deep.equal({ curr: 17, level: 9 });
+        s.add(100);
+        expect(curr(s)).to.deep.equal({ curr: 0, level: 10 });
+        s.add();
+        expect(curr(s)).to.deep.equal({ curr: 0, level: 10 });
+      });
+
       it('level', function() {
-        let s = new Skill('test level', 2, -1);
+        let s = new Skill('test level', 'desc level', 2, 10, -1);
         expect(curr(s)).to.deep.equal({ curr: 0, level: -1 });
         s.add(3);
         expect(curr(s)).to.deep.equal({ curr: 1, level: 0 });
